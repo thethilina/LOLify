5d5512cec6da3c6936221b6363cdc28f126ff713
Thilina Dewmina <thilinadewmina7@gmail.com>
2025-11-08 19:17:23 +0530

    navbarsidebarstuffs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "../public/components/NavBar";
+import SideNavBar from "../public/components/SideNavBar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+   
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      >        
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="">
       <NavBar/>
       <SideNavBar/>
-      
+     <Image src={noman} alt="feas"/> 
     </div>
   );
 }
